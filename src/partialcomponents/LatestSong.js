c5cdc0d60588ad3a7d8ce1ab787a5518c00ae93a
mahboobehjami-sh-a <mahboobehjami1380@gmail.com>
2023-01-12 08:57:04 +0330

    add component latest song

---
+++ src/partialcomponents/LatestSong.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import { Carousel, Container, Row, Col } from 'react-bootstrap';
+
+
+
+function LatestSong() {
+    const [songs, setSongs] = useState([])
+
+    useEffect(() => {
+    })
+
+    const showLatestSong = async () => {
+        var resultLatestSong = await fetch('https://api-beta.melobit.com/v1/song/new/0/11')
+            .then(Response =>
+                Response.json())
+            .then(data => {
+                setLatestSongs(data.results)
+            })
+    }
+    showLatestSong()
+
+
+    return (
+        <Container>
+            <Row className="justify-content-lg-center flex">
+
+                {
+                    songs.map((song, i) => (
+                        <img
+                            className='col-1'
+                            key={i}
+                            src={song.album.image.cover_small.url}
+                            alt={song.album.name}
+                        />
+                    ))
+                }
+
+            </Row>
+        </Container>
+    );
+}
+
+export default LatestSong;
